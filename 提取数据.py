a8ade28003dd8a277a66a6cd51527e6c3346456f
riverpath <643176574@qq.com>
2017-08-12 16:51:49 +0800

    baidu

--- 提取数据.py
+++ 提取数据.py
@@ -1,7 +1,7 @@
 import fetchtool.Asyncfetch as myscrapy
 import pandas as pd
-data=pd.read_excel(u'C:/Users/Administrator/Desktop/2017081014212131616845.xls','tuiguang')
+data=pd.read_excel(u'2017081014212131616845.xls','Sheet1')
 urls=["https://www.baidu.com/s?wd=" + str(i) + "%40v" for i in data[u'企业名称']]
 pstr=r'span class\="cust\-num"\>(.*?)\<'
-filename=u'数据/GDP.csv'
+filename=u'百度数据.csv'
 myscrapy.fetch_text(urls,pstr,filename)
