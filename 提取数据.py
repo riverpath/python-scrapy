e1c5b8730c3a9a39cc6f466b514a8ef868d96cc3
riverpath <643176574@qq.com>
2017-08-16 03:23:52 +0800

    通用爬虫

--- 提取数据.py
+++ 提取数据.py
@@ -2,14 +2,33 @@ import fetchtool.Asyncfetch as myscrapy
 import pandas as pd
 from urllib import request
 from urllib.parse import quote
+import re,json
 # 从api之中获取模块
-theme='job51'
+
+arg='http://s.hc360.com/?w=%CA%B3%C6%B7&mc=enterprise&ee=[[1,6,1]]&z=%D6%D0%B9%FA%3A%D5%E3%BD%AD%CA%A1'
+theme=u'慧聪公司'
+
+# xlst='xslt_bbs.xml'
+# print(apiurl)
+# 获取url地址
+class webclass(object):
+    def __init__(self):
+        self.pat=r'(.*?)\[(\[.*?\])\](.*)'
+    def seturlFromMem(self,url):
+        self.url=url
+        pattern=re.compile(self.pat)
+        gr=pattern.search(self.url)
+        ar=json.loads(gr.group(2))
+        self.urls=[gr.group(1) + str(i) + gr.group(3) for i in range(ar[0],ar[1],ar[2]) ]
+    def seturlFromFile(self,fileName):
+        self.fileName=fileName
+        df=pd.read_excel(fileName,0)
+        self.urls=df[URL]
+
 APIKey='9537dede351b975fa8e9dc67f57ea519'
 apiurl = "http://www.gooseeker.com/api/getextractor?key="+ APIKey +"&theme=" + quote(theme)
 xlst=request.urlopen(apiurl).read()
-# xlst='xslt_bbs.xml'
-
-# 获取url地址
-urls=["http://search.51job.com/list/000000,000000,0000,00,9,99,%25E9%2587%2591%25E8%259E%258D,2," + str(i) + ".html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=" for i in range(1,100)]
-
-myscrapy.fetch_text(urls,xlst,code='gbk')
+URLS=webclass()
+switch={0:lambda x:URLS.seturlFromFile(x),1:lambda x:URLS.seturlFromMem(x)}
+switch['http://' in arg](arg)
+myscrapy.fetch_text(URLS.urls,xlst,code='gbk')
